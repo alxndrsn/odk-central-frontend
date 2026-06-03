131027a86aa7139d5d6ad933bea03d30e1ffbfb0
alxndrsn <alxndrsn>
2026-06-03 08:30:34 +0000

    test if request.delete works

--- e2e-tests/global.teardown.js
+++ e2e-tests/global.teardown.js
@@ -8,6 +8,14 @@ const projectId = process.env.PROJECT_ID;
 const projectIdEncrypted = process.env.PROJECT_ID_ENCRYPTED;
 
 teardown('delete project', async ({ request }) => {
+  const check1 = await request.get(`${appUrl}/v1/projects`, {
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Basic ${credentials}`
+    }
+  });
+  console.log('check1:', await check1.json());
+
   const promises = [ projectId, projectIdEncrypted ].map((project) => {
     return request.delete(`${appUrl}/v1/projects/${project}`, {
       headers: {
@@ -18,6 +26,15 @@ teardown('delete project', async ({ request }) => {
       .then(res => res.json())
       .then(res => res.success);
   });
+
   const results = await Promise.allSettled(promises);
   results.forEach(result => expect(result.value).toEqual(true));
+
+  const check2 = await request.get(`${appUrl}/v1/projects`, {
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Basic ${credentials}`
+    }
+  });
+  console.log('check2:', await check2.json());
 });
